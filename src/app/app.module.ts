f510cb2779ffb41d892c7bdecb26faa20a041dd0
JIiCeHoK <foxs29546@gmail.com>
2022-12-15 20:01:54 +0500

    Add: подготовил заранее все компоненты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilterComponent } from '../components/filter/filter.component';
+import { EntityComponent } from '../components/entity/entity.component';
+import { ListItemComponent } from './list-item/list-item.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterComponent,
+    EntityComponent,
+    ListItemComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
